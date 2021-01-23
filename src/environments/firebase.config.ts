07f9b665064856d8bf6a89ba91739b08c40f4259
W.W Arunoda Isuruwan <arunoda2011cs112@gmail.com>
2021-01-23 11:08:11 +0530

    initial commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBT00FBj2VLqJD3RUbcbM8lpEEo0UJ1uEo",
+    authDomain: "osishoppingcart.firebaseapp.com",
+    databaseURL: "https://osishoppingcart-default-rtdb.firebaseio.com",
+    projectId: "osishoppingcart",
+    storageBucket: "osishoppingcart.appspot.com",
+    messagingSenderId: "918971880023",
+    appId: "1:918971880023:web:8c27d50e05765d692a0912",
+    measurementId: "G-L9BH1XB4X2"
+};
