53fa8054234fa128a46097d572eb5ec783164679
W.W Arunoda Isuruwan <arunoda2011cs112@gmail.com>
2021-01-23 11:32:39 +0530

    add firebase

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,10 +1,9 @@
 export const FireBaseConfig = {
-  apiKey: "AIzaSyBT00FBj2VLqJD3RUbcbM8lpEEo0UJ1uEo",
-    authDomain: "osishoppingcart.firebaseapp.com",
-    databaseURL: "https://osishoppingcart-default-rtdb.firebaseio.com",
-    projectId: "osishoppingcart",
-    storageBucket: "osishoppingcart.appspot.com",
-    messagingSenderId: "918971880023",
-    appId: "1:918971880023:web:8c27d50e05765d692a0912",
-    measurementId: "G-L9BH1XB4X2"
+  apiKey: "AIzaSyBrn-ZaQZj3MEz8gUtIM_tNwOMwUE3L3yI",
+  authDomain: "uclobbers-smart-shopping.firebaseapp.com",
+  projectId: "uclobbers-smart-shopping",
+  storageBucket: "uclobbers-smart-shopping.appspot.com",
+  messagingSenderId: "247679180305",
+  appId: "1:247679180305:web:a26b455600c193e3025bb0",
+  measurementId: "G-RVNNDYYF7M"
 };
