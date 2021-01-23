a07793643126e439f69d270188b44a49c5756ff7
W.W Arunoda Isuruwan <arunoda2011cs112@gmail.com>
2021-01-23 21:58:36 +0530

    change models

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -3,3 +3,5 @@ export * from './favourite.product';
 export * from './product';
 export * from './user';
 export * from './brand';
+export * from './colour';
+export * from './size';
--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -1,14 +1,27 @@
+import { Size } from './';
+import { Colour } from './';
 export class Product {
   $key: string;
   productId: number;
   productName: string;
+  productCategoryId: number;
   productCategory: string;
-  productPrice: number;
   productDescription: string;
+  genderType: number;
+  gender: string;
+  productPrice: number;
   productImageUrl: string;
   productAdded: number;
   productQuatity: number;
   ratings: number;
   favourite: boolean;
   productSeller: string;
+  material: string;
+  materialComposition: string;
+  liningMaterial: string;
+  colour: Colour[];
+  sizeType: number;
+  size: Size;
+  modelDetails: string;
+  isProductAvailable: boolean;
 }
