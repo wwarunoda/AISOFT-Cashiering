c0d4335eebba8729c111d54f2c47a5455f914d5c
Udara Bandara <udarab@lbfinance.lk>
2021-01-23 12:33:25 +0530

    firebase config

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -4,7 +4,7 @@ export const FireBaseConfig = {
   databaseURL:
     "https://uclobbers-smart-shopping-de515-default-rtdb.firebaseio.com",
   projectId: "uclobbers-smart-shopping-de515",
-  storageBucket: "gs://uclobbers-smart-shopping-de515.appspot.com",
+  storageBucket: "uclobbers-smart-shopping-de515.appspot.com",
   messagingSenderId: "887868938730",
   appId: "1:887868938730:web:6f223320c21e3e5aedfa93",
   measurementId: "G-SSDB2G4D0V",
