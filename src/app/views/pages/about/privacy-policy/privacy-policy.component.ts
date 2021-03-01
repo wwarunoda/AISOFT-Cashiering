44b10a4f10dd584d38a1ef59ded02bade41b53c1
W.W Arunoda Isuruwan <arunoda2011cs112@gmail.com>
2021-03-01 23:12:37 +0530

    add privacy policy details

--- src/app/views/pages/about/privacy-policy/privacy-policy.component.ts
+++ src/app/views/pages/about/privacy-policy/privacy-policy.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./privacy-policy.component.scss"],
 })
 export class PrivacyPolicyComponent implements OnInit {
+  config: any;
+  collection = { count: 5, data: [] };
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  for (let i = 1; i <= 5; i++) {
+    this.collection.data.push(
+      i
+    );
+  }
+
+  this.config = {
+    itemsPerPage: 1,
+    currentPage: 1,
+    totalItems: this.collection.count
+  };
+  }
+
+  pageChanged(event){
+    this.config.currentPage = event;
+  }
 }
